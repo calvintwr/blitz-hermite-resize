96606201c876c8e6022f89597edd209d06aa63cd
calvintwr <calvin14@gmail.com>
2015-12-05 16:37:29 +0800

    major refactoring

--- hermite-worker.js
+++ hermite-worker.js
@@ -1,4 +1,4 @@
-onmessage = function (event){
+onmessage = function (event) {
     var img = event.data[0];
     var data = img.data;
     var W = event.data[1];
@@ -13,39 +13,41 @@ onmessage = function (event){
     var ratio_w_half = Math.ceil(ratio_w/2);
     var ratio_h_half = Math.ceil(ratio_h/2);
 
-    for(var j = 0; j < H2; j++){
-        for(var i = 0; i < W2; i++){
+    for(var j = 0; j < H2; j++) {
+        for(var i = 0; i < W2; i++) {
             var x2 = (i + j*W2) * 4;
             var weight = 0;
             var weights = 0;
             var gx_r = gx_g = gx_b = gx_a = 0;
             var center_y = (j + 0.5) * ratio_h;
-            for(var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++){
+            for(var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++) {
                 var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
                 var center_x = (i + 0.5) * ratio_w;
                 var w0 = dy*dy //pre-calc part of w
-                for(var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++){
+                for(var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++) {
                     var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
                     var w = Math.sqrt(w0 + dx*dx);
                     if(w >= -1 && w <= 1){
                         //hermite filter
                         weight = 2 * w*w*w - 3*w*w + 1;
-                        if(weight > 0){
+                        if(weight > 0) {
                             dx = 4*(xx + yy*W);
                             gx_r += weight * data[dx];
                             gx_g += weight * data[dx + 1];
                             gx_b += weight * data[dx + 2];
                             gx_a += weight * data[dx + 3];
                             weights += weight;
-                            }
                         }
                     }
                 }
+            }
+
             data2[x2]     = gx_r / weights;
             data2[x2 + 1] = gx_g / weights;
             data2[x2 + 2] = gx_b / weights;
             data2[x2 + 3] = gx_a / weights;
-            }
+
         }
-    postMessage({data: img2});
-    };
+    }
+postMessage({data: img2});
+};
--- hermite.js
+++ hermite.js
@@ -1,61 +1,125 @@
 //name: Hermite resize
 //about: Fast image resize/resample using Hermite filter with JavaScript.
 //author: ViliusL
-//demo: http://viliusle.github.io/miniPaint/
-function resample_hermite(canvas, W, H, W2, H2){
-	var time1 = Date.now();
-	var img = canvas.getContext("2d").getImageData(0, 0, W, H);
-	var img2 = canvas.getContext("2d").getImageData(0, 0, W2, H2);
-	var data = img.data;
-	var data2 = img2.data;
-	var ratio_w = W / W2;
-	var ratio_h = H / H2;
-	var ratio_w_half = Math.ceil(ratio_w/2);
-	var ratio_h_half = Math.ceil(ratio_h/2);
-	
-	for(var j = 0; j < H2; j++){
-		for(var i = 0; i < W2; i++){
-			var x2 = (i + j*W2) * 4;
-			var weight = 0;
-			var weights = 0;
-			var weights_alpha = 0;
-			var gx_r = gx_g = gx_b = gx_a = 0;
-			var center_y = (j + 0.5) * ratio_h;
-			for(var yy = Math.floor(j * ratio_h); yy < (j + 1) * ratio_h; yy++){
-				var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
-				var center_x = (i + 0.5) * ratio_w;
-				var w0 = dy*dy //pre-calc part of w
-				for(var xx = Math.floor(i * ratio_w); xx < (i + 1) * ratio_w; xx++){
-					var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
-					var w = Math.sqrt(w0 + dx*dx);
-					if(w >= -1 && w <= 1){
-						//hermite filter
-						weight = 2 * w*w*w - 3*w*w + 1;
-						if(weight > 0){
-							dx = 4*(xx + yy*W);
-							//alpha
-							gx_a += weight * data[dx + 3];
-							weights_alpha += weight;
-							//colors
-							if(data[dx + 3] < 255)
-								weight = weight * data[dx + 3] / 250;
-							gx_r += weight * data[dx];
-							gx_g += weight * data[dx + 1];
-							gx_b += weight * data[dx + 2];
-							weights += weight;
-							}
-						}
-					}		
-				}
-			data2[x2]     = gx_r / weights;
-			data2[x2 + 1] = gx_g / weights;
-			data2[x2 + 2] = gx_b / weights;
-			data2[x2 + 3] = gx_a / weights_alpha;
-			}
-		}
-	console.log("hermite = "+(Math.round(Date.now() - time1)/1000)+" s");
-	canvas.getContext("2d").clearRect(0, 0, Math.max(W, W2), Math.max(H, H2));
-	canvas.width = W2;
-	canvas.height = H2;
-	canvas.getContext("2d").putImageData(img2, 0, 0);
-	}
+//forkedby: calvintwr
+var Hermite = {
+    init: function(workerPath) {
+        var copy = Object.create(this);
+        if (!workerPath) throw new Error('Fail to provide worker\'s path when initializing.');
+        copy._workerPath = workerPath;
+        copy.init = undefined;
+        return copy;
+    },
+    resize: function(obj, callback) {
+        var self = this;
+
+        var mandatory = ['source', 'width', 'height'];
+
+        for(var i=0; i<mandatory.length; i++) {
+            var param = mandatory[i];
+            if(typeof obj[param] === 'undefined') {
+                throw new Error('#resize() is expecting mandatory param `'+ param +'`.');
+            }
+
+        }
+
+        if(typeof callback !== 'function') {
+            throw new Error('#resize() is expecting `callback` to be a function.');
+        }
+
+        var sourceElement = this._extract(obj.source);
+        var mimeType = this._mimeConverter(obj.format);
+        var canvas = (sourceElement[1] === 'CANVAS') ? sourceElement[0] : this._imageToCanvas(sourceElement[0]);
+
+        var startTime = Date.now();
+
+        var originalWidth = canvas.width;
+        var originalHeight = canvas.height;
+        var resizeToWidth = obj.width;
+        var resizeToHeight = obj.height;
+
+        var original = canvas.getContext('2d').getImageData(0, 0, originalWidth, originalHeight);
+        var resizedImage = canvas.getContext('2d').getImageData(0, 0, resizeToWidth, resizeToHeight);
+        canvas.getContext('2d').clearRect(0, 0, originalWidth, originalHeight);
+
+        var worker = new Worker(self._workerPath);
+
+        worker.onmessage = function(event){
+
+            resizedImage = event.data.data;    
+
+            console.log('Hermite resize completed in ' + (Math.round(Date.now() - startTime)/1000) + 's');   
+            canvas.getContext('2d').clearRect(0, 0, resizeToWidth, resizeToHeight);
+            canvas.height = resizeToHeight;
+            canvas.width = resizeToWidth;
+            canvas.getContext('2d').putImageData(resizedImage, 0, 0);
+
+            if (obj.output === 'canvas') {
+                return callback(canvas);
+            } else if (obj.output === 'image') {
+                _makeIntoImage(self._canvas, self._mimeType, self._outputImageQuality, callback);   
+            } else if (typeof obj.output === 'undefined') {
+                // when not defined, assume whatever element type is the input, is the desired output
+                if(sourceElement[1] === 'IMG') return callback( self._canvasToImage(canvas, obj.mimeType, obj.quality) );
+                return callback(canvas);
+            } else {
+                throw new Error('`output` is not valid.');
+            }
+
+        };
+        worker.postMessage([original, originalWidth, originalHeight, resizeToWidth, resizeToHeight, resizedImage]);
+
+    },
+    _extract: function(source) {
+
+        var results;
+
+        if (source.tagName) {
+            // getElementById sources will pass this
+            results = [source, source.tagName];
+        } else if (source[0].tagName) {
+            results = [source[0], source[0].tagName];
+        } else {
+            throw new Error('#resize() found `source` element to be invalid.');
+        }
+
+        if (['CANVAS','IMG'].indexOf(results[1]) === -1) throw new Error('#resize() expects element type of `img` or `canvas`.');
+
+        return results;
+    },
+    _imageToCanvas: function(image) {
+
+        // create a off-screen canvas
+        var c = document.createElement('canvas');
+        var context = c.getContext('2d');
+
+        c.height = image.height;
+        c.width = image.width;
+        context.drawImage(image, 0, 0, image.width, image.height);
+
+        return c;
+
+    },
+    _canvasToImage: function(canvas, mimeType, quality, callback) {
+        var image = new Image();
+        image.src = canvas.toDataURL(mimeType, quality);
+        return image;
+    },
+    _mimeConverter: function(format) {
+
+        // if undefined, assume no compression.
+        if (typeof format === 'undefined') return 'image/png';
+
+        var formats = ['raw', 'png', 'jpg', 'gif'];
+        var index = formats.indexOf(format);
+
+        if (index === -1) throw new Error('#inputImage() outputType can only be `raw`, `png`, `jpg` or `gif`');
+
+        if (index === 0 || index === 1) return 'image/png';
+        if (index === 2) return 'image/jpg';
+        if (index === 3) return 'image/gif';
+
+        throw new Error('#_mimeConverter fell through all cases!');
+    }
+}
+
